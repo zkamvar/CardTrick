2f130c56fa27a594d4badd1162114f34aae00293
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2013-04-03 23:12:24 -0700

    Modified drawflip to the new system.

--- Aces.r
+++ Aces.r
@@ -23,21 +23,22 @@ deshuffle <- function(x){
 cut <- function(x){
   cutter <- sample(2:(ncol(x)-1), 1)
   x <- cbind(x[, cutter:ncol(x)], x[, 1:(cutter-1)])
-  cat("Cut at",cutter, ":", x, "\n")
+  cat("Cut at",cutter, ">>>\n", x, "\n")
   return(x)
 }
 
 drawflip <- function(x, divisor){
-  stops <- which(1:length(x)%%divisor == 0)
-  if(length(x)/divisor != length(stops))
+  stops <- which(1:ncol(x)%%divisor == 0)
+  if(ncol(x)/divisor != length(stops))
     stop ("Divisor is not an actual divisor of the length")
   flipornot <- function(x){
-    cat(x,"\n")
+    cat(x[2, ],"\n")
     cat("Do you want to flip? (y/n)")
     ans <- readLines(n=1)
     if(ans == "y"){
-      cat("OK:", -rev(x),"\n")
-      return(-rev(x))
+      cat("OK:", rev(x[1, ]),"\n")
+      x <- rbind(rev(x[2, ]), rev(x[1, ]))
+      return(x)
     }
     else if(ans == "n"){
       return(x)
@@ -47,7 +48,10 @@ drawflip <- function(x, divisor){
       flipornot(x)
     }
   }
-  return(unlist(lapply(stops, function(s) flipornot(x[(s-(divisor-1)):s]))))
+  newx <- x
+  lapply(stops, function(s) newx[, (s-(divisor-1)):s] <<- flipornot(x[, (s-(divisor-1)):s]))
+  cat("\n\nVoila!\n", newx,"\n")
+  return(newx)
 }
 
 twopile <- function(x){
